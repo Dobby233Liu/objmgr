2f8e1099b1e0e6a1104711a6896f084c4fca6531
Liu Wenyuan <15816141883@163.com>
2023-01-12 23:05:12 +0800

    Fix tests not running

--- include/test/prettyprint.h
+++ include/test/prettyprint.h
@@ -3,11 +3,12 @@
 #ifndef TEST_PRETTYPRINT_H
 #define TEST_PRETTYPRINT_H
 
-/* oops! all gnu extension */
+/* FIXME: oops! all gnu extension */
+
 #define PPRINT_FOR(s) s ": "
-#define PPRINT(o, p, m, x, ...) fprintf(o, m PPRINT_FOR(p) x "\n", ##__VA_ARGS__)
-#define INFO(m, ...) PPRINT(stdout, "info", PPRINT_FOR(m), __VA_ARGS__)
-#define ERR(m, ...) PPRINT(stderr, "err", PPRINT_FOR(m), __VA_ARGS__)
-#define WARN(m, ...) PPRINT(stderr, "warn", PPRINT_FOR(m), __VA_ARGS__)
+#define PPRINT(m, o, p, x, ...) fprintf(o, PPRINT_FOR(m) PPRINT_FOR(p) x "\n", ##__VA_ARGS__)
+#define INFO(m, ...) PPRINT(m, stdout, "info", __VA_ARGS__)
+#define ERR(m, ...) PPRINT(m, stderr, "err", __VA_ARGS__)
+#define WARN(m, ...) PPRINT(m, stderr, "warn", __VA_ARGS__)
 
 #endif /* TEST_PRETTYPRINT_H */
