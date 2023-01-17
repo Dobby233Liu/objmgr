a24920a551391d22d0ab0e9da65ba7d9ebe2a7c8
Liu Wenyuan <15816141883@163.com>
2023-01-17 14:52:30 +0800

    silenting it for clang

--- include/test/prettyprint.h
+++ include/test/prettyprint.h
@@ -3,10 +3,16 @@
 #ifndef TEST_PRETTYPRINT_H
 #define TEST_PRETTYPRINT_H
 
-/* FIXME: avoid ## */
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
+#endif
+/* FIXME: avoid ##, somehow */
 #define PPRINT(m, o, p, x, ...) fprintf(o, m ": " p ": " x "\n", ##__VA_ARGS__)
 #define INFO(m, ...) PPRINT(m, stdout, "info", __VA_ARGS__)
 #define ERR(m, ...) PPRINT(m, stderr, "err", __VA_ARGS__)
 #define WARN(m, ...) PPRINT(m, stderr, "warn", __VA_ARGS__)
-
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 #endif /* TEST_PRETTYPRINT_H */
