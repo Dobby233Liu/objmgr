120b6ee054bab73030ebedd016135ed1bbf3c412
Liu Wenyuan <15816141883@163.com>
2023-01-17 14:40:02 +0800

    I feel like I did nothing

--- include/test/prettyprint.h
+++ include/test/prettyprint.h
@@ -3,10 +3,8 @@
 #ifndef TEST_PRETTYPRINT_H
 #define TEST_PRETTYPRINT_H
 
-/* FIXME: oops! all gnu extension */
-
-#define PPRINT_FOR(s) s ": "
-#define PPRINT(m, o, p, x, ...) fprintf(o, PPRINT_FOR(m) PPRINT_FOR(p) x "\n", ##__VA_ARGS__)
+/* FIXME: avoid ## */
+#define PPRINT(m, o, p, x, ...) fprintf(o, m ": " p ": " x "\n", ##__VA_ARGS__)
 #define INFO(m, ...) PPRINT(m, stdout, "info", __VA_ARGS__)
 #define ERR(m, ...) PPRINT(m, stderr, "err", __VA_ARGS__)
 #define WARN(m, ...) PPRINT(m, stderr, "warn", __VA_ARGS__)
